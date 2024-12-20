c4a3050671f73c77a298fe9c50a579d403e3052a
Allan Da Silva <155674133+AllanSilvaP@users.noreply.github.com>
2024-12-20 19:56:45 -0300

    Add files via upload

---
+++ BarbeariaAguiaReal/front-end/src/js/index.js
@@ -0,0 +1,10 @@
+//funcao mostrar preco certo
+
+const precosPorDia = {
+    segunda: { corte: 25, barba: 25, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaDePele: 70, outro1: 25, outro2: 25},
+    terca: { corte: 25, barba: 25, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaDePele: 70, outro1: 25, outro2: 25},
+    quarta: { corte: 25, barba: 25, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaDePele: 70, outro1: 25, outro2: 25},
+    quinta: { corte: 30, barba: 30, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaDePele: 70, outro1: 30, outro2: 30},
+    sexta: { corte: 30, barba: 30, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaDePele: 70, outro1: 30, outro2: 30},
+    sabado: { corte: 30, barba: 30, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaDePele: 70, outro1: 30, outro2: 25},
+};
