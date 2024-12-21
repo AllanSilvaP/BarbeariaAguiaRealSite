dd4ed4bb16fb71b9f552f706cfd15e72b2e9b959
Allan Da Silva <155674133+AllanSilvaP@users.noreply.github.com>
2024-12-21 18:43:07 -0300

    Add files via upload
    Area Botoes funcionais

--- BarbeariaAguiaReal/front-end/src/js/index.js
+++ BarbeariaAguiaReal/front-end/src/js/index.js
@@ -1,10 +1,36 @@
 //funcao mostrar preco certo
 
 const precosPorDia = {
-    segunda: { corte: 25, barba: 25, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaDePele: 70, outro1: 25, outro2: 25},
-    terca: { corte: 25, barba: 25, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaDePele: 70, outro1: 25, outro2: 25},
-    quarta: { corte: 25, barba: 25, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaDePele: 70, outro1: 25, outro2: 25},
-    quinta: { corte: 30, barba: 30, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaDePele: 70, outro1: 30, outro2: 30},
-    sexta: { corte: 30, barba: 30, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaDePele: 70, outro1: 30, outro2: 30},
-    sabado: { corte: 30, barba: 30, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaDePele: 70, outro1: 30, outro2: 25},
+    segunda: { corte: 25, barba: 25, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaPele: 70, limpezaPele: 25, limpezaPele: 25},
+    terca: { corte: 25, barba: 25, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaPele: 70, limpezaPele: 25, limpezaPele: 25},
+    quarta: { corte: 25, barba: 25, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaPele: 70, limpezaPele: 25, limpezaPele: 25},
+    quinta: { corte: 30, barba: 30, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaPele: 70, limpezaPele: 30, limpezaPele: 30},
+    sexta: { corte: 30, barba: 30, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaPele: 70, limpezaPele: 30, limpezaPele: 30},
+    sabado: { corte: 30, barba: 30, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaPele: 70, limpezaPele: 30, limpezaPele: 25},
+    domingo: { corte: 30, barba: 30, pigmentacao: 15, progressiva: 70, luzes: 120, nevou: 200, limpezaPele: 70, limpezaPele: 30, limpezaPele: 25},
 };
+
+const btDias = document.querySelectorAll('.bt-servicos button');
+const servicos = document.querySelectorAll('.servico');
+
+function atualizaPrecos (diaSelecionado) {
+    const precos = precosPorDia[diaSelecionado];
+
+    servicos.forEach(servico => {
+        const nomeServico = servico.dataset.servico;
+        const precoCard = servico.querySelector('.preco-card')
+        precoCard.textContent = `R$${precos[nomeServico]}`;
+    });
+}
+
+btDias.forEach (botao => {
+    botao.addEventListener ('click', () => {
+        const diaSelecionado = botao.getAttribute('data-dia');
+        atualizaPrecos(diaSelecionado)
+    })
+})
+
+const hoje = new Date()
+const diasNomes = ["domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado"]
+const diaAtual = diasNomes [hoje.getDay()]
+atualizaPrecos(diaAtual); 
